8ad183c47f44b664bb3b3fd154357083008cdc13
Gabor Feher <feherga@gmail.com>
2017-10-04 00:36:02 +0200

    add some convolutional-layer-like thing

---
+++ cnn/convolutional_layer.cc
@@ -0,0 +1,31 @@
+#include "cnn/convolutional_layer.h"
+
+ConvolutionalLayer::ConvolutionalLayer(
+    int padding, int layers_per_image, int stride) : 
+        padding_(padding),
+        layers_per_image_(layers_per_image),
+        stride_(stride) {}
+
+void ConvolutionalLayer::Forward(const DeviceMatrix& input) {
+  input_ = input;
+  output_ = input
+      .AddPadding(padding_)
+      .Convolution(filters_, layers_per_image_, stride_);
+}
+
+void ConvolutionalLayer::Backward(const DeviceMatrix& output_gradients) {
+  // Below is some guesswork based on:
+  // (I have no idea if doing convolution in batches breaks that
+  // or not.)
+  // http://www.jefkine.com/general/2016/09/05/backpropagation-in-convolutional-neural-networks/
+  input_gradients_ = output_gradients
+      .AddPadding(padding_)
+      .Convolution(filters_.Rot180(), layers_per_image_, stride_);  // (22) TODO is f encoded in delta for me?
+  filters_gradients_ = input_
+      .Rot180()
+      .Convolution(output_gradients, layers_per_image_, 1);  // (14)
+}
+
+void ConvolutionalLayer::ApplyGradient(float learn_rate) {
+  filters_ = filters_.Add(filters_gradients_.Multiply(-learn_rate));
+}
---
+++ cnn/convolutional_layer.h
@@ -0,0 +1,22 @@
+#ifndef _CNN_CONVOLUTIONAL_LAYER_H_
+#define _CNN_CONVOLUTIONAL_LAYER_H_
+
+#include "cnn/layer.h"
+#include "linalg/device_matrix.h"
+
+class ConvolutionalLayer : public Layer {
+ public:
+  ConvolutionalLayer(int padding, int layers_per_image, int stride);
+  virtual void Forward(const DeviceMatrix& input);
+  virtual void Backward(const DeviceMatrix& output_gradients); 
+  virtual void ApplyGradient(float learn_rate);
+
+ private:
+  int padding_;
+  int layers_per_image_;
+  int stride_;
+  DeviceMatrix filters_;
+  DeviceMatrix filters_gradients_;
+};
+
+#endif  // _CNN_CONVOLUTIONAL_LAYER_H_
