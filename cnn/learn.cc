fa2b424d67ed87e3faae0c4d22b79d4c7199e93b
Gabor Feher <feherga@gmail.com>
2017-10-04 14:39:43 +0200

    add test data to test the convolutional layer

--- cnn/learn.cc
+++ cnn/learn.cc
@@ -10,50 +10,110 @@
 #include "linalg/device_matrix.h"
 
 int main() {
-  DeviceMatrix training_x(
-      DeviceMatrix(8, 3, (float[]) {
-          -1,  2, 1,
-           0,  1, 1,
-           1,  0, 1,
-           2, -1, 1,
-          -2,  1, 1,
-          -1,  0, 1,
-           0, -1, 1,
-           1, -2, 1,
-        }).T());
-  DeviceMatrix training_y(
-      DeviceMatrix(8, 1, (float[]) {
-          0,
-          0,
-          0,
-          0,
-          1,
-          1,
-          1,
-          1,
-      }).T());
+  // We want to teach the convolutional layer to detect two patterns:
+  // Patern1 (layer1+layer2):
+  // 100 001
+  // 010 010
+  // 001 100
+  // Pattern2 (layer1+layer2):
+  // 010 111
+  // 111 101
+  // 010 111
+  //
+  DeviceMatrix training_x(3, 6, 8 * 2, (float[]) {
+      // Image 1: pattern1 and pattern2 side by side
+      1, 0, 0, 0, 1, 0,  // layer1
+      0, 1, 0, 1, 1, 1,
+      0, 0, 1, 0, 1, 0,
+      0, 0, 1, 1, 1, 1,  // layer2
+      0, 1, 0, 1, 0, 1,
+      1, 0, 0, 1, 1, 1,
 
-  DeviceMatrix test_x(
-      DeviceMatrix(2, 3, (float[]) {
-          -1, -1, 1,
-           1,  1, 1,
-      }).T());
-  DeviceMatrix test_y(
-      DeviceMatrix(2, 1, (float[]) {
-          1,
-          0,
+      // Image 2: pattern2 and pattern1 side by side
+      0, 1, 0, 1, 0, 0, // layer1
+      1, 1, 1, 0, 1, 0,
+      0, 1, 0, 0, 0, 1,
+      1, 1, 1, 0, 0, 1, // layer2
+      1, 0, 1, 0, 1, 0,
+      1, 1, 1, 1, 0, 0,
+
+      // Image 3: pattern1 starting at the 3rd column
+      0, 0, 1, 0, 0, 0,  // layer1
+      1, 1, 0, 1, 0, 0,
+      0, 1, 0, 0, 1, 1,
+      1, 1, 0, 0, 1, 0,  // layer2
+      0, 0, 0, 1, 0, 0,
+      1, 0, 1, 0, 0, 1,
+
+      // Image 4: pattern2 starting at the 2nd column
+      0, 0, 1, 0, 0, 0,  // layer1
+      1, 1, 1, 1, 0, 0,
+      0, 0, 1, 0, 1, 1,
+      1, 1, 1, 1, 1, 0,  // layer2
+      0, 1, 0, 1, 0, 0,
+      1, 1, 1, 1, 0, 1,
+
+      // Image 5: pattern1 starting at the 4th column
+      0, 0, 1, 1, 0, 0,  // layer1
+      1, 1, 1, 0, 1, 0,
+      0, 0, 1, 0, 0, 1,
+      1, 1, 1, 0, 0, 1,  // layer2
+      0, 1, 0, 0, 1, 0,
+      1, 1, 1, 1, 0, 0,
+
+      // Image 6: pattern2 starting at the 1st column
+      0, 1, 0, 0, 0, 0,  // layer1
+      1, 1, 1, 1, 0, 0,
+      0, 1, 0, 0, 1, 1,
+      1, 1, 1, 1, 1, 0,  // layer2
+      1, 0, 1, 1, 0, 0,
+      1, 1, 1, 1, 0, 1,
+
+      // Image 7: garbage
+      0, 1, 0, 0, 0, 0,  // layer1
+      1, 0, 0, 1, 0, 0,
+      0, 1, 0, 0, 1, 1,
+      1, 1, 1, 1, 1, 0,  // layer2
+      1, 1, 0, 1, 0, 0,
+      1, 1, 1, 1, 0, 1,
+
+      // Image 8: garbage
+      0, 0, 1, 0, 0, 0,  // layer1
+      1, 1, 1, 1, 0, 0,
+      1, 1, 1, 0, 1, 1,
+      1, 1, 1, 1, 1, 1,  // layer2
+      0, 0, 1, 0, 0, 0,
+      1, 0, 1, 0, 0, 1,
+  });
+  DeviceMatrix training_y(
+      DeviceMatrix(8, 2, (float[]) {
+          1, 1,
+          1, 1,
+          1, 0,
+          0, 1,
+          1, 0,
+          0, 1,
+          0, 0,
+          0, 0,
       }).T());
 
   std::shared_ptr<LayerStack> stack = std::make_shared<LayerStack>();
   stack->AddLayer(std::make_shared<ConvolutionalLayer>(
-        5, 3, 3,
-        1, 3, 1));
+        2, 3, 3,
+        0, 2, 1));
+  // Output is supposed to be a bunch of row-vectors of length
+  // 4.
+  stack->AddLayer(std::make_shared<SigmoidLayer>());
+  // TODO: transpose output!
+  stack->AddLayer(std::make_shared<FullyConnectedLayer>(4, 2));
   stack->AddLayer(std::make_shared<SigmoidLayer>());
   Model model(
       stack,
       std::make_shared<ErrorLayer>());
 
   model.Train(training_x, training_y, 100, 5);
-  model.Evaluate(test_x, test_y);
+
+
+  //model.Evaluate(test_x, test_y);
 
 }
